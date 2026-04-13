293ab3ae383346d87fbf52bee15fc9c73ece81b2
Aditya Aryan <25f2001442@ds.study.iitm.ac.in>
2026-04-13 07:14:28 +0000

    Made changes to views.py and connected it to the url.py

---
+++ awpl/awpl/views.py
@@ -0,0 +1,11 @@
+from djngo.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello, World!")
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
